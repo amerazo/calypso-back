3a24a6de3b7bd0ad97a93a92f45c627857c16214
secretivo <secretivo@secretivos-Air.lan>
2023-05-20 02:21:10 -0400

    board.js reverse

--- controllers/board.js
+++ controllers/board.js
@@ -1,54 +1,60 @@
-const express = require('express');
-const Board = require('../models/board');
-const Task = require('../models/task');
-const Card = require('../models/card');
-const router = express.Router();
+const express = require('express')
+const Board = require('../models/board')
+const Task = require('../models/task')
+const Card = require('../models/card')
+const router = express.Router()
 
-// get
+
+
+
+//get
 router.get('/', async (req, res) => {
-  try {
-    res.json(await Board.find({})).status(200);
-  } catch (error) {
-    res.status(400).json(error);
-    console.log('error', error);
-  }
-});
+    try{
+        res.json(await Board.find({})).status(200)
+    } catch (error) {
+        res.status(400).json(error)
+        console.log('error', error)
+    }
+    // console.log(Board);
+})
 
 router.get('/:id', async (req, res) => {
-  try {
-    res.json(await Board.findById(req.params.id));
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+    try {
+        res.json(await Board.findById(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
-// post
+//post 
 router.post('/', async (req, res) => {
-  console.log(req.body);
-  try {
-    const createdBoard = await Board.create(req.body);
-    res.status(201).json(createdBoard);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+    console.log(req.body);
+    try{
+        res.json(await Board.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 
-// put
+//put 
 router.put('/:id', async (req, res) => {
-  try {
-    res.json(await Board.findByIdAndUpdate(req.params.id, req.body));
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+    try{
+        res.json(await Board.findByIdAndUpdate(req.params.id, req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+   
+})
 
-// delete
+//delete
 router.delete('/:id', async (req, res) => {
-  try {
-    res.json(await Board.findByIdAndRemove(req.params.id));
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+    try{
+        res.json(await Board.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+
 
 module.exports = router;
